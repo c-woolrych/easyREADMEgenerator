c1fb2a87292d506becbe1cda32e897a4bd2aeb10
c-woolrych <c.woolrych@gmail.com>
2024-02-20 14:52:03 +0000

    start the array of questions

--- index.js
+++ index.js
@@ -5,6 +5,35 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What would you like the title of your project to be?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description explaining what, why and how of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions for use. Include screenshots as needed.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license type from the list.',
+        choices: ['None', 'MIT', 'GNU GPLv3', 'CC0-1.0', 'SIL OpenFont License 1.1', 'GNU AGPLv3', 'GNU GPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'Boost Software License 1.0']
+    },
+    {
+        type: 
+    }
 
 ];
 
